4ad30dce6a3fafc9d5fafea2039111cf8c95b146
Pedro Mangabeira <pedro@easy-guild.net>
2015-04-14 17:47:24 +0200

    CAG and viper update

--- bin/cag-install.js
+++ bin/cag-install.js
@@ -1,3 +1,5 @@
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" --expose-gc --always-compact -- "$0" "$@"
 require('../lib/install')(process.argv, {name: 'cag', description: 'CAG Process Manager'}, function (err) {
     process.exit(err ? 1 : 0);
 });
--- bin/viper-install.js
+++ bin/viper-install.js
@@ -1,3 +1,5 @@
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" --expose-gc --always-compact -- "$0" "$@"
 require('../lib/install')(process.argv, {name: 'viper', description: 'Viper Process Manager'}, function (err) {
     process.exit(err ? 1 : 0);
 });
